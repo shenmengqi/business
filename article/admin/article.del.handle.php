294042ef24972a5776a38fdce46fb5ea4264c1c0
shenmengqi <2631946703@qq.com>
2016-10-07 22:09:06 +0800

    Add files via upload

---
+++ article/admin/article.del.handle.php
@@ -0,0 +1,12 @@
+<?php
+require_once('../connect.php');
+$id=$_GET['id'];
+$deletesql="delete from article where id=$id";
+if(mysqli_query($con,$deletesql)){
+	echo "<script>alert('删除文章成功');
+	window.location.href='';</script>";
+	}else{
+		echo "<script>alert('删除文章失败');
+		window.location.href='';</script>";
+		}
+?>
